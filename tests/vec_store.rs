39e0a3b9fb7b2e147b5fa07c986c7deea9322c26
adoerr <0xad@gmx.net>
2021-04-08 05:59:52 +0200

    more validate tests

--- tests/vec_store.rs
+++ tests/vec_store.rs
@@ -48,3 +48,42 @@ fn append_multiple_nodes() {
 
     assert_eq!(19, size);
 }
+
+#[test]
+fn validate_works() {
+    let mut s = VecStore::<E>::new();
+    let mut mmr = MerkleMountainRange::<E, VecStore<E>>::new(&mut s);
+    let mut size = 0;
+
+    (0..=2u8).for_each(|i| {
+        let n = vec![i, 10];
+        size = mmr.append(&n).unwrap();
+    });
+
+    assert_eq!(4, size);
+    assert!(mmr.validate().unwrap());
+
+    s = VecStore::<E>::new();
+    mmr = MerkleMountainRange::<E, VecStore<E>>::new(&mut s);
+    size = 0;
+
+    (0..=6u8).for_each(|i| {
+        let n = vec![i, 10];
+        size = mmr.append(&n).unwrap();
+    });
+
+    assert_eq!(11, size);
+    assert!(mmr.validate().unwrap());
+
+    s = VecStore::<E>::new();
+    mmr = MerkleMountainRange::<E, VecStore<E>>::new(&mut s);
+    size = 0;
+
+    (0..=10u8).for_each(|i| {
+        let n = vec![i, 10];
+        size = mmr.append(&n).unwrap();
+    });
+
+    assert_eq!(19, size);
+    assert!(mmr.validate().unwrap());
+}

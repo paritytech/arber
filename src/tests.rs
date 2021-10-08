ef5eb1e29b7b5e0c0af17c0ed80890df51314405
adoerr <0xad@gmx.net>
2021-10-08 18:35:40 +0200

    rework tests

--- src/tests.rs
+++ src/tests.rs
@@ -34,80 +34,86 @@ fn make_mmr(num_leafs: u8) -> MerkleMountainRange<E, VecStore<E>> {
 }
 
 #[test]
-fn append_two_nodes() {
+fn append_two_nodes() -> Result<(), Error> {
     let s = VecStore::<E>::new();
     let mut mmr = MerkleMountainRange::<E, VecStore<E>>::new(s);
 
     let n1 = vec![0u8, 10];
-    let pos = mmr.append(&n1).unwrap();
+    let pos = mmr.append(&n1)?;
 
     assert_eq!(1, pos);
 
     let n2 = vec![1u8, 10];
-    let pos = mmr.append(&n2).unwrap();
+    let pos = mmr.append(&n2)?;
 
     assert_eq!(3, pos);
+
+    Ok(())
 }
 
 #[test]
-fn append_tree_nodes() {
+fn append_tree_nodes() -> Result<(), Error> {
     let s = VecStore::<E>::new();
     let mut mmr = MerkleMountainRange::<E, VecStore<E>>::new(s);
 
     let n1 = vec![0u8, 10];
-    let pos = mmr.append(&n1).unwrap();
+    let pos = mmr.append(&n1)?;
 
     assert_eq!(1, pos);
 
     let n2 = vec![1u8, 10];
-    let pos = mmr.append(&n2).unwrap();
+    let pos = mmr.append(&n2)?;
 
     assert_eq!(3, pos);
 
     let n3 = vec![2u8, 10];
-    let pos = mmr.append(&n3).unwrap();
+    let pos = mmr.append(&n3)?;
 
     assert_eq!(4, pos);
+
+    Ok(())
 }
 
 #[test]
-fn validate_works() {
+fn validate_works() -> Result<(), Error> {
     let s = VecStore::<E>::new();
     let mut mmr = MerkleMountainRange::<E, VecStore<E>>::new(s);
 
     // empty MMR is valid
-    assert!(mmr.validate().unwrap());
+    assert!(mmr.validate()?);
 
     let n1 = vec![0u8, 10];
-    let mut size = mmr.append(&n1).unwrap();
+    let mut size = mmr.append(&n1)?;
 
     assert_eq!(1, size);
-    assert!(mmr.validate().unwrap());
+    assert!(mmr.validate()?);
 
     let n2 = vec![1u8, 10];
-    size = mmr.append(&n2).unwrap();
+    size = mmr.append(&n2)?;
 
     assert_eq!(3, size);
-    assert!(mmr.validate().unwrap());
+    assert!(mmr.validate()?);
 
     let n3 = vec![2u8, 10];
-    size = mmr.append(&n3).unwrap();
+    size = mmr.append(&n3)?;
 
     assert_eq!(4, size);
-    assert!(mmr.validate().unwrap());
+    assert!(mmr.validate()?);
+
+    Ok(())
 }
 
 #[test]
-fn validate_fails() {
+fn validate_fails() -> Result<(), Error> {
     let mut mmr = make_mmr(3);
 
     let want = Error::InvalidNodeHash(
         2,
-        Hash::from_hex("0x00000000000000000000000000000000").unwrap(),
-        Hash::from_hex("0x9f7d5dc4ed828ec11a1101e7a047dd7d").unwrap(),
+        Hash::from_hex("0x00000000000000000000000000000000")?,
+        Hash::from_hex("0x9f7d5dc4ed828ec11a1101e7a047dd7d")?,
     );
 
-    mmr.store.hashes[2] = Hash::from_hex("0x00").unwrap();
+    mmr.store.hashes[2] = Hash::from_hex("0x00")?;
     let got = mmr.validate().err().unwrap();
 
     // compare the actual error messages
@@ -120,11 +126,11 @@ fn validate_fails() {
 
     let want = Error::InvalidNodeHash(
         6,
-        Hash::from_hex("0x00000000000000000000000000000000").unwrap(),
-        Hash::from_hex("0x2cabe06f9728067daa538c96edbd6ef7").unwrap(),
+        Hash::from_hex("0x00000000000000000000000000000000")?,
+        Hash::from_hex("0x2cabe06f9728067daa538c96edbd6ef7")?,
     );
 
-    mmr.store.hashes[6] = Hash::from_hex("0x00").unwrap();
+    mmr.store.hashes[6] = Hash::from_hex("0x00")?;
     let got = mmr.validate().err().unwrap();
 
     // compare the actual error messages
@@ -132,6 +138,8 @@ fn validate_fails() {
     let got = format!("{}", got);
 
     assert_eq!(want, got);
+
+    Ok(())
 }
 
 #[test]
@@ -145,36 +153,38 @@ fn proof_fails() {
 }
 
 #[test]
-fn proof_works() {
+fn proof_works() -> Result<(), Error> {
     let mmr = make_mmr(2);
-    let proof = mmr.proof(1).unwrap();
+    let proof = mmr.proof(1)?;
 
     assert_eq!(3, proof.mmr_size);
     assert_eq!(1, proof.path.len());
-    assert_eq!(mmr.hash(2).unwrap(), proof.path[0]);
+    assert_eq!(mmr.hash(2)?, proof.path[0]);
 
     let mmr = make_mmr(4);
-    let proof = mmr.proof(4).unwrap();
+    let proof = mmr.proof(4)?;
 
     assert_eq!(7, proof.mmr_size);
     assert_eq!(2, proof.path.len());
-    assert_eq!(mmr.hash(5).unwrap(), proof.path[0]);
-    assert_eq!(mmr.hash(3).unwrap(), proof.path[1]);
+    assert_eq!(mmr.hash(5)?, proof.path[0]);
+    assert_eq!(mmr.hash(3)?, proof.path[1]);
 
     let mmr = make_mmr(11);
-    let proof = mmr.proof(5).unwrap();
+    let proof = mmr.proof(5)?;
 
     assert_eq!(19, proof.mmr_size);
     assert_eq!(4, proof.path.len());
-    assert_eq!(mmr.hash(4).unwrap(), proof.path[0]);
-    assert_eq!(mmr.hash(3).unwrap(), proof.path[1]);
-    assert_eq!(mmr.hash(14).unwrap(), proof.path[2]);
+    assert_eq!(mmr.hash(4)?, proof.path[0]);
+    assert_eq!(mmr.hash(3)?, proof.path[1]);
+    assert_eq!(mmr.hash(14)?, proof.path[2]);
 
-    let h1 = mmr.hash(18).unwrap();
-    let h2 = mmr.hash(19).unwrap();
+    let h1 = mmr.hash(18)?;
+    let h2 = mmr.hash(19)?;
     let h = (h1, h2).hash();
     let h = hash_with_index(mmr.size, &h);
     assert_eq!(h, proof.path[3]);
+
+    Ok(())
 }
 
 #[test]
@@ -204,71 +214,75 @@ fn partial_prove_works() -> Result<(), Error> {
 }
 
 #[test]
-fn bag_lower_peaks_works() {
+fn bag_lower_peaks_works() -> Result<(), Error> {
     let mmr = make_mmr(2);
     let got = mmr.bag_lower_peaks(3);
 
     assert_eq!(None, got);
 
     let mmr = make_mmr(3);
-    let want = mmr.hash(4).unwrap();
+    let want = mmr.hash(4)?;
     let got = mmr.bag_lower_peaks(3).unwrap();
 
     assert_eq!(want, got);
 
     let mmr = make_mmr(7);
-    let h1 = mmr.hash(10).unwrap();
-    let h2 = mmr.hash(11).unwrap();
+    let h1 = mmr.hash(10)?;
+    let h2 = mmr.hash(11)?;
     let want = (h1, h2).hash();
     let want = hash_with_index(mmr.size, &want);
     let got = mmr.bag_lower_peaks(7).unwrap();
 
     assert_eq!(want, got);
+
+    Ok(())
 }
 
 #[test]
-fn peak_path_works() {
+fn peak_path_works() -> Result<(), Error> {
     let mmr = make_mmr(2);
     let path = mmr.peak_path(3);
 
     assert!(path.is_empty());
 
     let mmr = make_mmr(3);
-    let want = mmr.hash(4).unwrap();
+    let want = mmr.hash(4)?;
     let want = vec![want];
     let got = mmr.peak_path(3);
 
     assert_eq!(want, got);
 
-    let want = mmr.hash(3).unwrap();
+    let want = mmr.hash(3)?;
     let want = vec![want];
     let got = mmr.peak_path(4);
 
     assert_eq!(want, got);
 
     let mmr = make_mmr(7);
-    let h1 = mmr.hash(10).unwrap();
-    let h2 = mmr.hash(7).unwrap();
+    let h1 = mmr.hash(10)?;
+    let h2 = mmr.hash(7)?;
     let want = vec![h1, h2];
     let got = mmr.peak_path(11);
 
     assert_eq!(want, got);
 
-    let h1 = mmr.hash(11).unwrap();
-    let h2 = mmr.hash(7).unwrap();
+    let h1 = mmr.hash(11)?;
+    let h2 = mmr.hash(7)?;
     let want = vec![h1, h2];
     let got = mmr.peak_path(10);
 
     assert_eq!(want, got);
 
-    let h1 = mmr.hash(11).unwrap();
-    let h2 = mmr.hash(10).unwrap();
+    let h1 = mmr.hash(11)?;
+    let h2 = mmr.hash(10)?;
     let want = (h2, h1).hash();
     let want = hash_with_index(mmr.size, &want);
     let want = vec![want];
     let got = mmr.peak_path(7);
 
     assert_eq!(want, got);
+
+    Ok(())
 }
 
 #[test]
@@ -288,120 +302,128 @@ fn hash_error_works() {
 }
 
 #[test]
-fn hash_works() {
+fn hash_works() -> Result<(), Error> {
     let mmr = make_mmr(3);
 
     let h1 = hash_with_index(0, &vec![0u8, 10].hash());
-    let h = mmr.hash(1).unwrap();
+    let h = mmr.hash(1)?;
     assert_eq!(h, h1);
 
     let h2 = hash_with_index(1, &vec![1u8, 10].hash());
-    let h = mmr.hash(2).unwrap();
+    let h = mmr.hash(2)?;
     assert_eq!(h, h2);
 
     let h3 = hash_with_index(2, &(h1, h2).hash());
-    let h = mmr.hash(3).unwrap();
+    let h = mmr.hash(3)?;
     assert_eq!(h, h3);
 
     let h4 = hash_with_index(3, &vec![2u8, 10].hash());
-    let h = mmr.hash(4).unwrap();
+    let h = mmr.hash(4)?;
     assert_eq!(h, h4);
 
     let mmr = make_mmr(4);
 
     let h1 = hash_with_index(3, &vec![2u8, 10].hash());
-    let h = mmr.hash(4).unwrap();
+    let h = mmr.hash(4)?;
     assert_eq!(h, h1);
 
     let h2 = hash_with_index(4, &vec![3u8, 10].hash());
-    let h = mmr.hash(5).unwrap();
+    let h = mmr.hash(5)?;
     assert_eq!(h, h2);
 
     let h3 = hash_with_index(5, &(h1, h2).hash());
-    let h = mmr.hash(6).unwrap();
+    let h = mmr.hash(6)?;
     assert_eq!(h, h3);
+
+    Ok(())
 }
 
 #[test]
-fn peaks_works() {
+fn peaks_works() -> Result<(), Error> {
     let mmr = make_mmr(1);
     let peaks = mmr.peaks();
 
     assert!(peaks.len() == 1);
-    assert_eq!(mmr.hash(mmr.size).unwrap(), peaks[0]);
+    assert_eq!(mmr.hash(mmr.size)?, peaks[0]);
 
     let mmr = make_mmr(2);
     let peaks = mmr.peaks();
 
     assert!(peaks.len() == 1);
-    assert_eq!(mmr.hash(mmr.size).unwrap(), peaks[0]);
+    assert_eq!(mmr.hash(mmr.size)?, peaks[0]);
 
     let mmr = make_mmr(4);
     let peaks = mmr.peaks();
 
     assert!(peaks.len() == 1);
-    assert_eq!(mmr.hash(mmr.size).unwrap(), peaks[0]);
+    assert_eq!(mmr.hash(mmr.size)?, peaks[0]);
 
     let mmr = make_mmr(10);
     let peaks = mmr.peaks();
 
     assert!(peaks.len() == 2);
-    assert_eq!(mmr.hash(15).unwrap(), peaks[0]);
-    assert_eq!(mmr.hash(18).unwrap(), peaks[1]);
+    assert_eq!(mmr.hash(15)?, peaks[0]);
+    assert_eq!(mmr.hash(18)?, peaks[1]);
 
     let mmr = make_mmr(11);
     let peaks = mmr.peaks();
 
     assert!(peaks.len() == 3);
-    assert_eq!(mmr.hash(15).unwrap(), peaks[0]);
-    assert_eq!(mmr.hash(18).unwrap(), peaks[1]);
-    assert_eq!(mmr.hash(19).unwrap(), peaks[2]);
+    assert_eq!(mmr.hash(15)?, peaks[0]);
+    assert_eq!(mmr.hash(18)?, peaks[1]);
+    assert_eq!(mmr.hash(19)?, peaks[2]);
+
+    Ok(())
 }
 
 #[test]
-fn root_works() {
+fn root_works() -> Result<(), Error> {
     let mmr = make_mmr(1);
-    let root = mmr.root().unwrap();
-    let hash = mmr.hash(1).unwrap();
+    let root = mmr.root()?;
+    let hash = mmr.hash(1)?;
 
     assert_eq!(root, hash);
 
     let mmr = make_mmr(2);
-    let root = mmr.root().unwrap();
-    let hash = mmr.hash(3).unwrap();
+    let root = mmr.root()?;
+    let hash = mmr.hash(3)?;
 
     assert_eq!(root, hash);
 
     let mmr = make_mmr(4);
-    let root = mmr.root().unwrap();
-    let hash = mmr.hash(7).unwrap();
+    let root = mmr.root()?;
+    let hash = mmr.hash(7)?;
 
     assert_eq!(root, hash);
 
     let mmr = make_mmr(6);
-    let root = mmr.root().unwrap();
-    let h1 = mmr.hash(7).unwrap();
-    let h2 = mmr.hash(10).unwrap();
+    let root = mmr.root()?;
+    let h1 = mmr.hash(7)?;
+    let h2 = mmr.hash(10)?;
     let hash = hash_with_index(mmr.size, &(h1, h2).hash());
 
     assert_eq!(root, hash);
 
     let mmr = make_mmr(11);
-    let root = mmr.root().unwrap();
-    let h1 = mmr.hash(18).unwrap();
-    let h2 = mmr.hash(19).unwrap();
+    let root = mmr.root()?;
+    let h1 = mmr.hash(18)?;
+    let h2 = mmr.hash(19)?;
     let h2 = hash_with_index(mmr.size, &(h1, h2).hash());
-    let h1 = mmr.hash(15).unwrap();
+    let h1 = mmr.hash(15)?;
     let hash = hash_with_index(mmr.size, &(h1, h2).hash());
 
     assert_eq!(root, hash);
+
+    Ok(())
 }
 
 #[test]
-fn root_fails() {
+fn root_fails() -> Result<(), Error> {
     let s = VecStore::<E>::new();
     let mmr = MerkleMountainRange::<E, VecStore<E>>::new(s);
-    let root = mmr.root().unwrap();
+    let root = mmr.root()?;
 
     assert_eq!(ZERO_HASH, root);
+
+    Ok(())
 }
